28af70e2f60e31bb15bfb9e29a48b749a5973dea
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-16 21:32:58 +0300

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tree = document.getElementsByClassName('tree')[0];
+const headers = document.getElementsByTagName('li');
+
+for (const li of headers) {
+  if (li.firstElementChild) {
+    const span = document.createElement('span');
+
+    span.className = 'header';
+    span.innerHTML = li.firstChild.textContent;
+    li.firstChild.remove();
+
+    li.prepend(span);
+  }
+}
+
+tree.addEventListener('click', function (e) {
+  const target = e.target.closest('span');
+
+  if (!target) {
+    return;
+  }
+
+  if (target.parentElement.lastElementChild.hidden) {
+    target.parentElement.lastElementChild.hidden = false;
+  } else {
+    target.parentElement.lastElementChild.hidden = true;
+  }
+});
